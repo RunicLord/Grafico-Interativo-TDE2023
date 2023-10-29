f56861d66d9c7ecbd219655b15e556d64365cd67
RunicLord <78120104+RunicLord@users.noreply.github.com>
2023-10-28 21:02:00 -0300

    Update Menu_principal.py

--- Menu_principal.py
+++ Menu_principal.py
@@ -4,16 +4,40 @@ st.set_page_config(
     page_title="TDE - Grafico Dinâmico"
 )
 
-st.write("Sobre")
-
 st.sidebar.success("Selecione uma aba acima")
 
+st.title(":red[COMO O SERVIÇO DE STREAMING PODE TER AFETADO O CINEMA NACIONAL]")
+st.divider()
+st.subheader("**Esse aplicativo tem como objetivo facilitar a visualização dos dados citados no trabalho.**")
 st.markdown(
     """
-    # Titulo genérico
-    Isso é só um texto genérico pra encher a página e manter tudo
-    mais visualmente agradável.
     
-    Isso é mais texto pra preencher a página.
+    A chegada dos serviços de Streaming de vídeo foi algo inovador no mercado mundial que trouxe conforto e praticidade 
+    para seus usuários, com esse cenário visamos analisar se esse mercado pode ter causado impactos negativos ao seu 
+    concorrente já consolidado no Brasil: o cinema. 
+    
+    Buscando compreender as mudanças que podem ter ocorrido na produção e distribuição dos filmes nacionais e se houve 
+    uma queda em suas produções através de dados abertos disponibilizados pelo governo, pesquisas e empresas privadas. 
+    Visando analisar esse cenário de extrema importância para orientar políticas públicas, estratégias de mercado e 
+    incentivar debates futuros sobre o cinema nacional e outros serviços de entretenimento que ao decorrer do tempo 
+    foram se tornando obsoletos.
+    
+    Para essa pesquisa, nosso objetivo é obter informações referentes ao crescimento ou queda da popularidade de cinemas 
+    e serviços de streaming, para isso foi necessário a pesquisa por datasets confiáveis que possam abranger um pouco o 
+    nosso conhecimento e criar uma pesquisa mais profunda e detalhada levando também em consideração outras vertentes 
+    desse mercado como: A pandemia do COVID-19 que afetou o mundo inteiro por cerca de 3 anos e levou os cinemas a um 
+    fechamento temporário no qual os efeitos ainda são completamente entendidos. Partindo disso, podemos dar início a 
+    uma tentativa de compreender o momento atual da indústria cinematográfica no Brasil.
+    
+    Esse trabalho tem como objeto realizar uma pesquisa de cunho exploratório, de abordagem quantitativa, tendo como seu
+    principal objetivo a pesquisa e coleta de dados referente não só sobre o serviço de streaming e o cinema mas também 
+    foi pesquisado dados sobre domicílios com acesso a internet, para traçar uma relação com o assunto tema proposto 
+    pelos grupo de estudantes. Foi utilizado essencialmente o motor de busca Google para buscar por websites com bases 
+    de dados que poderiam servir para essa pesquisa, ao mesmo tempo que esses mesmos dados deveriam ser confiáveis e 
+    legítimos, forma buscados sites como o do governo federal, netflix e outros sites especializados em realizar 
+    pesquisas dos mais diversos temas para assim criar uma análise comparativa e interpretativa para responder a 
+    pergunta que foi estabelecida no início de nossa pesquisa.
     """
 )
+
+st.divider()
